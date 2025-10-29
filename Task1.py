f0c9a1b2990edc8efbbb8d5714dca6f8cefe1ee0
bhajanpreet465 <62834198+bhajanpreet465@users.noreply.github.com>
2025-10-29 22:32:19 +0530

    Assignment 1 - Submission  !!
    This is the completion of task 1 and task 2

---
+++ Task1.py
@@ -0,0 +1,12 @@
+a = int(input("Please Enter the First number : "))
+b = int(input("Please Enter the Second number : "))
+
+Addition =  (a+b )
+Substraction = (a-b)
+Multiply =  (a*b )
+Division = (a/b )
+
+print("Addition: "+ str(Addition))
+print("Substraction: "+ str(Substraction))
+print("Multiply: "+ str(Multiply))
+print("Division: "+ str(Division))
